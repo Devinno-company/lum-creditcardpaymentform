7e4e7cf567c4e36c0f16c476eeec49ee818e78a6
Devinno-company <72108018+Devinno-company@users.noreply.github.com>
2020-12-10 04:13:20 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 process.title = 'LUM | Pagamento por cartão de crédito';
-let args = process.argv,
-port = args[2] || 7070,
+
+port = process.env.PORT || 7070,
 webServer = require('./server');
 
 webServer.listen(port, function() {
